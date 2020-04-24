b9db740f27c854a61e69fd13729af6564bc7a961
christianinacio <christianginacio@gmail.com>
2020-04-23 23:03:06 -0300

    Agrego Game-Card

---
+++ Game-Card/Game-Card.h
@@ -0,0 +1,15 @@
+#ifndef GAME_CARD_H_
+#define GAME_CARD_H_
+
+#include<commons/config.h>
+#include<commons/log.h>
+#include<commons/string.h>
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<readline/readline.h>
+#include<netdb.h>
+
+int crear_conexion(char* ip, char* puerto);
+
+#endif
