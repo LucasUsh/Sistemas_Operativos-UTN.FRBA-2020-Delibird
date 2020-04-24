72f930a315f3d5794cbb376f7a0e2da318b40648
Lautaro Blanco <lautaro.blancobordenabe@gmail.com>
2020-04-24 19:36:58 -0300

    clases de algunos datos (a revisar)

--- team/src/team.h
+++ team/src/team.h
@@ -10,6 +10,44 @@
 #include <stdlib.h>
 #include <commons/string.h>
 #include <commons/log.h>
+#include <commons/config.h>
+#include <commons/collections/list.h>
+
+
+typedef enum {
+	RR = 1,
+	SJFSD = 2,
+	SJFCD = 3
+} algoritmo_code;
+
+typedef enum {
+	NEW = 1,
+	READY = 2,
+	BLOCKED = 3,
+	EXEC = 4,
+	EXIT = 5
+} estado_code;
+
+typedef struct
+{
+	char* position;
+	t_list* pokemones;
+	t_list* objetivo;
+	estado_code estado;
+	algoritmo_code algoritmo;
+
+} t_trainer;
+
+typedef struct {
+	char* nombre;
+	int cantidad;
+} t_pokemon;
+
+typedef struct {
+	t_pokemon* pokemones;
+} t_objetivo;
+
+
 #define TEAM_H_
 
 
