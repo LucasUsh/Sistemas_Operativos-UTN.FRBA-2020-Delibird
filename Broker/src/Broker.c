4c11b14c27f2bcdfc308b3e721086940c13f56b2
MatiMon <matimon@est.frba.utn.edu.ar>
2020-04-27 19:38:25 -0300

    Broker recibe y devuelve mensajes

---
+++ Broker/src/Broker.c
@@ -0,0 +1,16 @@
+/*
+ ============================================================================
+ Name        : Broker.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include "Broker.h"
+
+int main(void) {
+	iniciar_servidor();
+	return EXIT_SUCCESS;
+}
---
+++ Broker/src/Broker.h
@@ -0,0 +1,18 @@
+/*
+ * Broker.h
+ *
+ *  Created on: 27 abr. 2020
+ *      Author: utnso
+ */
+
+#ifndef BROKER_H_
+#define BROKER_H_
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<commons/log.h>
+#include "/home/utnso/workspace/tp-2020-1c-5rona/OurLibraries/Sockets/socketsServidor.c"
+
+
+#endif /* BROKER_H_ */
