50336de8f01ea0be4d976cd129dd8b4185a23f3b
christianinacio <christianginacio@gmail.com>
2020-05-02 17:42:33 -0300

    Creo socket del game boy

--- Game-Boy/src/Game-Boy.c
+++ Game-Boy/src/Game-Boy.c
@@ -1,16 +1,59 @@
 #include "Game-Boy.h"
 
+t_log* logger;
+t_config* config;
+
 int main(void)
 {
-	t_config* config;
-	t_log* logger;
+	int socketBroker;
+	int socketTeam;
 
 	logger = log_create("/home/utnso/workspace/tp-2020-1c-5rona/Game-Boy/Game-Boy.log", "Game-Boy", 1, LOG_LEVEL_INFO);
 	config = config_create("/home/utnso/workspace/tp-2020-1c-5rona/Game-Boy/Game-Boy.config");
 
 	printf("GAME BOY iniciando ... \n");
+	socketBroker = conexionBroker();
+	socketTeam = conexionTeam();
 
+
+
+
+	liberar_conexion(socketBroker);
+	liberar_conexion(socketTeam);
 	config_destroy(config);
 	log_destroy(logger);
 	return 0;
+}
+
+int conexionBroker()
+{
+	char* ip_broker = config_get_string_value(config,"IP_BROKER​");
+	char* puerto_broker = config_get_string_value(config,"PUERTO_BROKER");
+
+	int socket = crear_conexion(ip_broker,puerto_broker);
+	if(socket != 0)
+	{
+		log_info(logger,"Conectado al Broker");
+	}else
+	{
+		log_info(logger,"Error al conectar al Broker");
+	}
+
+	return socket;
+}
+
+int conexionTeam()
+{
+	char* ip_team = config_get_string_value(config,"IP_TEAM");
+	char* puerto_team = config_get_string_value(config,"PUERTO_TEAM​");
+
+	int socket = crear_conexion(ip_team, puerto_team);
+	if(socket != 0)
+	{
+		log_info(logger,"Conectado al Team");
+	}else
+	{
+		log_info(logger,"Error al conectar al Team");
+	}
+	return socket;
 }
--- Game-Boy/src/Game-Boy.h
+++ Game-Boy/src/Game-Boy.h
@@ -5,7 +5,9 @@
 #include<commons/config.h>
 #include<commons/log.h>
 #include "/home/utnso/workspace/tp-2020-1c-5rona/OurLibraries/Sockets/socketsUnificado.c"
+#include<readline/readline.h>
 
-
+int conexionBroker();
+int conexionTeam();
 
 #endif
