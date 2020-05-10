1e310d22d1987e140b9c860eefbf1be66e23dbec
christianinacio <christianginacio@gmail.com>
2020-05-10 17:10:25 -0300

    Arreglo argumentos del Game Boy

--- Game-Boy/src/Game-Boy.c
+++ Game-Boy/src/Game-Boy.c
@@ -5,33 +5,40 @@ t_config* config;
 
 int main(int argc, char *argv[])
 {
-	int socketBroker;
-	int socketTeam;
+	int socket;
 
 	logger = log_create("/home/utnso/workspace/tp-2020-1c-5rona/Game-Boy/Game-Boy.log", "Game-Boy", 1, LOG_LEVEL_INFO);
 	config = config_create("/home/utnso/workspace/tp-2020-1c-5rona/Game-Boy/Game-Boy.config");
 
-	printf("GAME BOY iniciando ... \n",argv[1]);
-	switch(*argv[1]){
-		case 'B': //Broker
-			socketBroker = conexionBroker();
-		break;
-		case 'T': //Team
-			socketTeam = conexionTeam();
-		break;
-		case 'G': //Game-Card
+	printf("GAME BOY iniciando ... \n");
 
-		break;
-		default:
-			printf("Sin proceso \n");
-		break;
+	if(string_contains(argv[1], "BROKER")){
+		socket = conexionBroker();
+		if(socket == 0){
+			return 0;
+		}
+		if(string_contains(argv[2], "NEW_POKEMON")){
+			enviar_new_pokemon(argv[3], argv[4], argv[5], argv[6], socket);
+		}
+		if(string_contains(argv[2], "APPEARED_POKEMON")){
+			printf("Appeared Pokemon \n");
+		}
+		if(string_contains(argv[2], "CATCH_POKEMON")){
+			printf("Catch Pokemon \n");
+		}
+		if(string_contains(argv[2], "CAUGHT_POKEMON")){
+			printf("Caught Pokemon \n");
+		}
+	}
+
+	if(strcmp(argv[1], "TEAM")== 0){
+		socket = conexionTeam();
 	}
 
 
 
 
-	liberar_conexion(socketBroker);
-	liberar_conexion(socketTeam);
+	liberar_conexion(socket);
 	config_destroy(config);
 	log_destroy(logger);
 	return 0;
@@ -69,3 +76,22 @@ int conexionTeam()
 	}
 	return socket;
 }
+
+void enviar_new_pokemon(char* pokemon, char* x, char* y, char* cantidad, int socket_cliente)
+{
+	t_pokemon *p_pokemon = (t_pokemon*) malloc(sizeof(t_pokemon));
+	p_pokemon -> nombre = pokemon;
+
+	t_posicion *p_posicion = (t_posicion*) malloc(sizeof(t_posicion));
+	p_posicion -> X = (int)x;
+	p_posicion -> Y = (int)y;
+
+	t_New *p_New = (t_New*) malloc(sizeof(t_New));
+	p_New -> cant = (int)cantidad;
+	//p_New -> pokemon = p_pokemon;
+	//p_New -> posicion = p_posicion;
+
+	printf("Envio pokemon \n");
+
+}
+
