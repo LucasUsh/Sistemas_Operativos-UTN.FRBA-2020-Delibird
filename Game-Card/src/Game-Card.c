ed5039ada4b0213efd494ddf5b43bd8ad896e8e5
LucasUsh <lucaas-e@hotmail.com>
2020-05-14 20:27:50 -0300

    Pequeñas modificaciones y agregados + base para servidor Game Card

--- Game-Card/src/Game-Card.c
+++ Game-Card/src/Game-Card.c
@@ -1,23 +1,26 @@
 #include "Game-Card.h"
 
-t_log* logger;
-t_config* config;
-
 int main(void)
 {
+	pthread_t hilo_servidor_game_card;
+	crear_servidor_game_card(&hilo_servidor_game_card);
+
 	pthread_t h1;
 	int socket;
-    logger = log_create("/home/utnso/workspace/tp-2020-1c-5rona/Game-Card/Game-Card.log", "Game-Card", 1, LOG_LEVEL_INFO);
+	logger_game_card = log_create("/home/utnso/workspace/tp-2020-1c-5rona/Game-Card/Game-Card.log", "Game-Card", 1, LOG_LEVEL_INFO);
 
-    printf("GAME CARD iniciando ... \n");
     pthread_create(&h1, NULL, conexionBroker, &socket);
 
 
     pthread_join(h1,NULL);
     liberar_conexion(socket);
-    config_destroy(config);
-    log_destroy(logger);
+    config_destroy(config_game_card);
+    log_destroy(logger_game_card);
     return 0;
+}
+
+void crear_servidor_game_card(pthread_t* hilo_servidor_game_card) {
+
 }
 
 void conexionBroker(int *socket)
@@ -25,10 +28,10 @@ void conexionBroker(int *socket)
 	char* ip_broker;
 	char* puerto_broker;
 	char* timer;
-	config = config_create("/home/utnso/workspace/tp-2020-1c-5rona/Game-Card/Game-Card.config");
-	ip_broker = config_get_string_value(config,"IP_BROKER");
-	puerto_broker = config_get_string_value(config,"PUERTO_BROKER");
-	timer = config_get_string_value(config,"TIEMPO_DE_REINTENTO_CONEXION");
+	config_game_card = config_create("/home/utnso/workspace/tp-2020-1c-5rona/Game-Card/Game-Card.config");
+	ip_broker = config_get_string_value(config_game_card,"IP_BROKER");
+	puerto_broker = config_get_string_value(config_game_card,"PUERTO_BROKER");
+	timer = config_get_string_value(config_game_card,"TIEMPO_DE_REINTENTO_CONEXION");
 
 	*socket = crear_conexion(ip_broker,puerto_broker);
 	while(*socket == 0)
@@ -37,5 +40,5 @@ void conexionBroker(int *socket)
 		*socket = crear_conexion(ip_broker,puerto_broker);
 	}
 	printf("\n");
-	log_info(logger,"Conectado al Broker");
+	log_info(logger_game_card,"Conectado al Broker");
 }
--- Game-Card/src/Game-Card.h
+++ Game-Card/src/Game-Card.h
@@ -11,6 +11,14 @@
 #include<pthread.h>
 #include "../../OurLibraries/Sockets/sockets.c"
 
+#define IP_GAME_CARD "127.0.0.3"
+#define PUERTO_GAME_CARD "5001"
+
+
+t_log* logger_game_card;
+t_config* config_game_card;
+
+crear_servidor_game_card(pthread_t*);
 void conexionBroker(int *socket);
 
 #endif
