492a82582c769bafb8979b9d9b59bccfd9162179
MatiMon <matimon@est.frba.utn.edu.ar>
2020-06-07 18:16:24 -0300

    Eliminado id_mensaje de los structs de los mensajes

--- OurLibraries/UniversoPokemon/universo.h
+++ OurLibraries/UniversoPokemon/universo.h
@@ -20,7 +20,7 @@
 typedef struct {
 	uint32_t X;
 	uint32_t Y;
-}t_posicion;
+}t_posicion; // ocupa 8 bytes
 
 typedef struct {
 	uint32_t size_Nombre;
@@ -28,6 +28,8 @@ typedef struct {
 	uint32_t cantidad; // creo que no deberia estar pero no quiero romper nada de Team asi que lo dejo :p
 	t_posicion* posicion; //supongo que pasará lo mismo con esto jajajaja
 } t_pokemon;
+// con esta estructura ocupa maximo 26 bytes, minimo 19 bytes
+// nombre mas corto: Mew/Muk (3 bytes); nombre mas largo: Feraligatr (10 bytes)
 
 
 
@@ -43,38 +45,33 @@ typedef struct {
 /****************************************/
 
 typedef struct {
-	uint32_t id_mensaje;
 	t_pokemon pokemon;
 	t_posicion posicion;
 	uint32_t cant;
 } t_New; // codigo_operacion= 1
+// ocupa entre 35 y 42 bytes
 
 typedef struct {
-	uint32_t id_mensaje;
 	t_pokemon pokemon;
 	uint32_t cantUbicaciones;
 	t_list * listaPosiciones;
 } t_Localized; // codigo_operacion= 2
 
 typedef struct {
-	uint32_t id_mensaje;
 	t_pokemon pokemon;
 } t_Get; // codigo_operacion= 3
 
 typedef struct {
-	uint32_t id_mensaje;
 	t_pokemon pokemon;
 	t_posicion posicion;
 } t_Appeared; // codigo_operacion= 4
 
 typedef struct {
-	uint32_t id_mensaje;
 	t_pokemon pokemon;
 	t_posicion posicion;
 } t_Catch; // codigo_operacion= 5
 
 typedef struct {
-	uint32_t id_mensaje;
 	uint32_t fueAtrapado; // 1 cuando fue atrapado, 0 cuando no fue atrapado
 } t_Caught; // codigo_operacion= 6
 
