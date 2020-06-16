8aa287bf0708d9b5f0edbe97a035f14508adff09
LucasUsh <lucaas-e@hotmail.com>
2020-06-16 14:40:48 -0300

    Fix minor error

--- Game-Boy/src/Game-Boy.c
+++ Game-Boy/src/Game-Boy.c
@@ -32,7 +32,7 @@ int main(int argc, char *argv[])
 		}
 	}
 	// Ejemplo./gameboy GAMECARD NEW_POKEMON Pikachu 2 5 10 9
-	else { if(string_contains(argv[1], "GAMECARD")) {
+	else if(string_contains(argv[1], "GAMECARD")) {
 		socket = conexionGameCard();
 		if(socket == 0){
 			log_info(logger,"Error al conectar con Game-Card");
@@ -51,7 +51,7 @@ int main(int argc, char *argv[])
 			printf("Caught Pokemon \n");
 		}
 	}
-	else{
+	else {
 
 		if(string_contains(argv[1], "TEAM")){
 			socket = conexionTeam();
