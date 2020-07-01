95625b8ea5cf35ef1e143aacd962c1939218c2e0
christianinacio <christianginacio@gmail.com>
2020-07-01 20:18:35 -0300

    arreglo manejo de memoria new pokemon

--- Game-Boy/src/mensajes.c
+++ Game-Boy/src/mensajes.c
@@ -6,31 +6,27 @@ void enviar_new_pokemon(char* pokemon, char* x, char* y, char* cantidad, int32_t
 	paquete->codigo_operacion = NEW_POKEMON;
 	paquete->buffer = malloc(sizeof(t_buffer));
 
-	t_posicion * posicion = malloc(sizeof(t_posicion));
-	posicion->X = (int32_t) atoi (x);
-	posicion->Y = (int32_t) atoi (y);
+	t_posicion  posicion;
+	posicion.X = (int32_t) atoi (x);
+	posicion.Y = (int32_t) atoi (y);
 
-	t_pokemon * p_pokemon = malloc(sizeof(t_pokemon));
-	p_pokemon->size_Nombre = strlen(pokemon) +1;
-	p_pokemon->nombre = pokemon;
+	t_pokemon p_pokemon;
+	p_pokemon.size_Nombre = strlen(pokemon) +1;
+	p_pokemon.nombre = pokemon;
 
-	t_New * new = malloc(sizeof(t_New));
-	new->cant = (int32_t) atoi (cantidad);
-	new->posicion = *posicion;
-	new->pokemon = *p_pokemon;
+	t_New new;
+	new.cant = (int32_t) atoi (cantidad);
+	new.posicion = posicion;
+	new.pokemon = p_pokemon;
 
-	paquete->buffer->size = tamanio_new (new);
+	paquete->buffer->size = tamanio_new (&new);
 	paquete->buffer->id_Mensaje = 0;
-	paquete->buffer->stream = new;
+	paquete->buffer->stream = &new;
 
 	int32_t bytes_a_enviar;
-	void *paqueteSerializado = serializar_paquete_new (paquete, &bytes_a_enviar, new);
+	void *paqueteSerializado = serializar_paquete_new (paquete, &bytes_a_enviar, &new);
 
 	send(socket_cliente, paqueteSerializado, bytes_a_enviar, 0);
-
-	free(posicion);
-	free(p_pokemon);
-	free(new);
 
 	free(paqueteSerializado);
 	free(paquete->buffer);
