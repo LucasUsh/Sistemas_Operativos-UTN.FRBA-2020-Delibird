ce62c4efb4dba06e8e52450197e7ddb973fc4a42
christianinacio <christianginacio@gmail.com>
2020-08-02 12:43:15 -0300

    modifico script para mostrar contenido de pokemones

--- scripts/pruebasGame-Card.sh
+++ scripts/pruebasGame-Card.sh
@@ -1,8 +1,15 @@
 MONTAJE=/home/utnso/Escritorio/TallGrass
+FILES=/home/utnso/Escritorio/TallGrass/Files
 
 if [ -d "$MONTAJE" ]; then
-	echo "pokemones"
-	ls /home/utnso/Escritorio/TallGrass/Files
+	for Pokemon in "$FILES"/*
+	do
+		if [ -d "${Pokemon}" ]; then
+			echo "-----${Pokemon}:"
+			cat "${Pokemon}/Metadata.bin"
+			echo -ne "\n"
+		fi
+	done
 else
 	echo "no existe $MONTAJE"
 fi
